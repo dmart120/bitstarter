105fc4d29c560463f4c958731f20fa51158f49e0
Ubuntu <ubuntu@ip-10-154-138-123.ec2.internal>
2013-07-12 00:45:56 +0000

    updated grader.js to be able to read from url per hw3 part3.  also added results.json which was created by grader.js after running it to check my heroku url

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -22,16 +23,19 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
+var url = require('url');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +53,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +65,42 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkURL = function(result) {
+    if(result instanceof Error) {
+	console.log(result.message);
+    } else {
+	var outfile = 'test.html';
+	fs.writeFileSync(outfile, result);
+	var checkJson = checkHtmlFile(outfile, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	var outJsonFile = 'results.json';
+	fs.writeFileSync(outJsonFile, outJson);
+	console.log(outJson);
+    }
+};
+
+var urltoString = function(URL) {
+    var stringURL = url.format(URL);
+    return stringURL;
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to html file')
+	.parse(process.argv);
+    if(program.url != null) {
+	var urlString = urltoString(program.url);
+	rest.get(urlString).on('complete', function(result) {
+	    checkURL(result);
+
+	});
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
